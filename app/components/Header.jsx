e8918604b3fafe0c715351d939b2eb40fb550dec
Jagath <jagathbotheju@gmail.com>
2023-03-31 12:21:30 +0300

    add dark mode

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from "react";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
+import DarkModeSwitch from "./DarkModeSwitch";
 import MenuItem from "./MenuItem";
 
 export default function Header() {
@@ -10,14 +11,20 @@ export default function Header() {
       <div className="flex max-w-6xl sm:mx-auto items-center py-6 justify-between">
         <Link href="/">
           <h2 className="text-2xl">
-            <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">IMDb</span>
+            <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">
+              IMDb
+            </span>
             <span className="text-xl hidden sm:inline">Clone</span>
           </h2>
         </Link>
-        <div className="flex">
-            
-        <MenuItem title="HOME" address="/" Icon={AiFillHome} />
-        <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
+        <div className="flex items-center">
+          <MenuItem title="HOME" address="/" Icon={AiFillHome} />
+          <MenuItem
+            title="ABOUT"
+            address="/about"
+            Icon={BsFillInfoCircleFill}
+          />
+          <DarkModeSwitch />
         </div>
       </div>
     </>
--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,24 @@
 import Header from './components/Header'
 import './globals.css'
+import Providers from './Providers'
+
 
 export const metadata = {
   title: 'IMDb Movies',
-  description: 'IMDB movie clonegit ',
+  description: 'IMDB movie clone git',
+  
 }
 
 export default function RootLayout({ children }) {
+
   return (
     <html lang="en">
       <body>
+        <Providers>
+
         <Header/>
         {children}
+        </Providers>
         </body>
     </html>
   )
